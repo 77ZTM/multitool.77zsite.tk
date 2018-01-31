53952adb6dab8596f15177001186dc43bb0cd295
Vince <r2d2fan20@outlook.com>
2018-01-30 19:40:43 -0600

    first commit

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>
+			The Multitool
+		</title>
+		<style type="text/css">
+			body
+			{
+				background: black;
+				perspective: 300px;
+			}
+
+
+			div
+			{
+				display: block;
+				margin: 300px auto;
+				height: 150px;
+				width: 10%;
+				text-align: center;
+				animation: logo 5s linear infinite;
+			}
+
+			@keyframes logo
+			{
+				0% { transform: rotateY(0deg); }
+				100% { transform: rotateY(360deg); }
+			}
+		</style>
+	</head>
+	<body>
+		<div class="container">
+			<h1 style="color: white;">
+				The MultiTool
+			</h1>
+		</div>
+	</body>
+</html>
